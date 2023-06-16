4a21e710ac899036e3c2e825bc01eeb06428a9a4
BenjaminRomero10 <programming.benja@gmail.com>
2023-06-16 09:27:54 -0500

    Primer commit para enlace con VM

---
+++ php/login.php
@@ -0,0 +1,14 @@
+<?php
+$serverName = "ASUSZEPHYRUS\SQLEXPRESS";
+
+$connectionInfo = array( "Database"=>"StarbucksSTSS");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+
+if( $conn ) {
+     echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
