ed8d9f065204137624bcffe3e49dfa69f01c2df1
BenjaminRomero10 <programming.benja@gmail.com>
2023-06-18 19:04:58 -0500

    phpTest

--- php/login.php
+++ php/login.php
@@ -1,23 +1,30 @@
 <?php
+    $user = $_POST ['user'];
+    $pass = $_POST ['pass'];
     $serverName = "stss.database.windows.net";
     $connectionOptions = array(
         "Database" => "STSSDB",
         "Uid" => "CloudSA4049f354",
         "PWD" => "Contraseña11"
     );
+
     //Establishes the connection
     $conn = sqlsrv_connect($serverName, $connectionOptions);
     if ($conn === false) {
         die(print_r(sqlsrv_errors(), true));
     }
-    $tsql= "SELECT * FROM Sucursal";
+
+    echo("Credenciales: " . $user . " " . $pass . "<br/>");
+
+    $tsql= "EXEC ObtenerContraseña" . "'" . $user . "';";
     $getResults= sqlsrv_query($conn, $tsql);
-    echo ("Reading data from table" . PHP_EOL);
+
     if ($getResults == FALSE)
         echo (sqlsrv_errors());
-    while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
-     echo ($row['Nombre'] . " " . $row['Contra'] . PHP_EOL);
-}
-sqlsrv_free_stmt($getResults);
+
+    // while ($row = sqlsrv_fetch_array($getResults, SQLSRV_FETCH_ASSOC)) {
+    //  echo ($row['Nombre'] . " " . $row['Contra'] . PHP_EOL);
+    // }
+    sqlsrv_free_stmt($getResults);
 ?>
 
