3a7900eeb02cbc44f9c1b5c98920c7068662fbc2
BenjaminRomero10 <programming.benja@gmail.com>
2023-06-21 23:33:12 -0500

    phpTest14

--- js/login.js
+++ js/login.js
@@ -1,24 +1,32 @@
-var url = "../php/login.php";
+var formulario = document.getElementById("formulario-login");
+var user = document.getElementById("user").value;
+var pass = document.getElementById("pass").value;
 
-var opciones = {
-    method: "POST",
-    body: new URLSearchParams({
-        user: $("#user").val(),
-        pass: $("#pass").val()
-    }),
-    header: {
-        "Content-Type" : "application/x-www-form-urlencoded"
-    }
-};
 
-fetch(url, opciones).then(function(respuesta){
-    return respuesta.json();
-}).then(function(data){
-    if (data.status == "ok"){
-        window.location.href = "../index.html";
-    } else {
-        alert(data.message);
-    }
-}).catch(function(e){
-    alert(e);
+formulario.addEventListener("submit", function(event) {
+  event.preventDefault();
+  var url = "../php/login.php";
+
+  var opciones = {
+      method: "POST",
+      body: new URLSearchParams({
+          user: user,
+          pass: pass
+      }),
+      headers: {
+          "Content-Type": "application/x-www-form-urlencoded"
+      }
+  };
+  
+  fetch(url, opciones).then(function(respuesta){
+      return respuesta.json();
+  }).then(function(data){
+      if (data.status == "ok"){
+          window.location.href = "../index.html";
+      } else {
+          alert(data.message);
+      }
+  }).catch(function(e){
+      alert(e);
+  });
 });
