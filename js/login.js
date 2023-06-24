75d45e1a8fbe4070388fdaf3684eb340742ddc3d
BenjaminRomero10 <programming.benja@gmail.com>
2023-06-24 00:05:57 -0500

    phpTest25

--- js/login.js
+++ js/login.js
@@ -2,17 +2,14 @@ window.onload = function(){
     let user = sessionStorage.getItem('user');
     console.log(user);
     
-    if(user != null){
-        Toastify({
-            text: "Bienvenido, has iniciado sesión correctamente.",
-            duration: 3000,
-            close: true,
-            gravity: "top",
-            position: "right",
-            backgroundColor: "#1e3932",
-            stopOnFocus: true,
-        }).showToast();
-    }
-
+    Toastify({
+        text: "Bienvenido, has iniciado sesión correctamente.",
+        duration: 3000,
+        close: true,
+        gravity: "top",
+        position: "right",
+        backgroundColor: "#1e3932",
+        stopOnFocus: true,
+    }).showToast();
 }
     
