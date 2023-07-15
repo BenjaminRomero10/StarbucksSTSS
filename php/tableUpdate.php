2e4288f8d9869eaa43fb00eef9503a1c895d491b
BenjaminRomero10 <programming.benja@gmail.com>
2023-07-14 22:15:42 -0500

    alpha13

--- php/tableUpdate.php
+++ php/tableUpdate.php
@@ -1,7 +1,6 @@
 <?php
     session_start();
     // Recibir los datos del producto y la acción
-    $producto = $_POST['producto'];
     $accion = $_POST['accion'];
     $serverName = "stss.database.windows.net";
     $connectionOptions = array(
@@ -14,63 +13,72 @@
         die(print_r(sqlsrv_errors(), true));
     }
 
-    switch ($accion) {
-    case "PAPELERIA":
-        $sql = "SELECT s.ProductoID AS ID, p.NombreProducto AS Nombre, s.CantidadPasada AS UltimoPedido, s.FechaSuministro AS Fecha, s.Stock AS Stock, s.UnidadMedida AS UnidadMedida
-                FROM Suministros s
-                INNER JOIN Productos p ON s.ProductoID = p.ProductoID
-                INNER JOIN ProductosEnSucursal ps ON s.ProductoID = ps.ProductoID AND s.SucursalID = ps.SucursalID
-                INNER JOIN Sucursal su ON s.SucursalID = su.SucursalID
-                WHERE su.Nombre = '{$_SESSION['user']}' AND p.FamiliaProducto = 'PAPELERIA';
-                ";
-        break;
-    case "SECOS":
-        $sql = "SELECT s.ProductoID AS ID, p.NombreProducto AS Nombre, s.CantidadPasada AS UltimoPedido, s.FechaSuministro AS Fecha, s.Stock AS Stock, s.UnidadMedida AS UnidadMedida
-                FROM Suministros s
-                INNER JOIN Productos p ON s.ProductoID = p.ProductoID
-                INNER JOIN ProductosEnSucursal ps ON s.ProductoID = ps.ProductoID AND s.SucursalID = ps.SucursalID
-                INNER JOIN Sucursal su ON s.SucursalID = su.SucursalID
-                WHERE su.Nombre = '{$_SESSION['user']}' AND p.FamiliaProducto = 'SECOS';
-                ";
-        break;
-    case "EPP":
-        $sql = "SELECT s.ProductoID AS ID, p.NombreProducto AS Nombre, s.CantidadPasada AS UltimoPedido, s.FechaSuministro AS Fecha, s.Stock AS Stock, s.UnidadMedida AS UnidadMedida
-                FROM Suministros s
-                INNER JOIN Productos p ON s.ProductoID = p.ProductoID
-                INNER JOIN ProductosEnSucursal ps ON s.ProductoID = ps.ProductoID AND s.SucursalID = ps.SucursalID
-                INNER JOIN Sucursal su ON s.SucursalID = su.SucursalID
-                WHERE su.Nombre = '{$_SESSION['user']}' AND p.FamiliaProducto = 'EPP';
-                ";
-        break;
-    case "LIMPIEZA":
-        $sql = "SELECT s.ProductoID AS ID, p.NombreProducto AS Nombre, s.CantidadPasada AS UltimoPedido, s.FechaSuministro AS Fecha, s.Stock AS Stock, s.UnidadMedida AS UnidadMedida
-                FROM Suministros s
-                INNER JOIN Productos p ON s.ProductoID = p.ProductoID
-                INNER JOIN ProductosEnSucursal ps ON s.ProductoID = ps.ProductoID AND s.SucursalID = ps.SucursalID
-                INNER JOIN Sucursal su ON s.SucursalID = su.SucursalID
-                WHERE su.Nombre = '{$_SESSION['user']}' AND p.FamiliaProducto = 'LIMPIEZA';
-                ";
-        break;
-    case "SMALLWARE":
-        $sql = "SELECT s.ProductoID AS ID, p.NombreProducto AS Nombre, s.CantidadPasada AS UltimoPedido, s.FechaSuministro AS Fecha, s.Stock AS Stock, s.UnidadMedida AS UnidadMedida
-                FROM Suministros s
-                INNER JOIN Productos p ON s.ProductoID = p.ProductoID
-                INNER JOIN ProductosEnSucursal ps ON s.ProductoID = ps.ProductoID AND s.SucursalID = ps.SucursalID
-                INNER JOIN Sucursal su ON s.SucursalID = su.SucursalID
-                WHERE su.Nombre = '{$_SESSION['user']}' AND p.FamiliaProducto = 'SMALLWARE';
-                ";
-        break;
-    case "PRENDAS":
-        $sql = "SELECT s.ProductoID AS ID, p.NombreProducto AS Nombre, s.CantidadPasada AS UltimoPedido, s.FechaSuministro AS Fecha, s.Stock AS Stock, s.UnidadMedida AS UnidadMedida
-                FROM Suministros s
-                INNER JOIN Productos p ON s.ProductoID = p.ProductoID
-                INNER JOIN ProductosEnSucursal ps ON s.ProductoID = ps.ProductoID AND s.SucursalID = ps.SucursalID
-                INNER JOIN Sucursal su ON s.SucursalID = su.SucursalID
-                WHERE su.Nombre = '{$_SESSION['user']}' AND p.FamiliaProducto = 'PRENDAS DE VESTIR';
-                ";
-        break;
-    }
+    // switch ($accion) {
+    // case "PAPELERIA":
+    //     $sql = "SELECT s.ProductoID AS ID, p.NombreProducto AS Nombre, s.CantidadPasada AS UltimoPedido, s.FechaSuministro AS Fecha, s.Stock AS Stock, s.UnidadMedida AS UnidadMedida
+    //             FROM Suministros s
+    //             INNER JOIN Productos p ON s.ProductoID = p.ProductoID
+    //             INNER JOIN ProductosEnSucursal ps ON s.ProductoID = ps.ProductoID AND s.SucursalID = ps.SucursalID
+    //             INNER JOIN Sucursal su ON s.SucursalID = su.SucursalID
+    //             WHERE su.Nombre = '{$_SESSION['user']}' AND p.FamiliaProducto = 'PAPELERIA';
+    //             ";
+    //     break;
+    // case "SECOS":
+    //     $sql = "SELECT s.ProductoID AS ID, p.NombreProducto AS Nombre, s.CantidadPasada AS UltimoPedido, s.FechaSuministro AS Fecha, s.Stock AS Stock, s.UnidadMedida AS UnidadMedida
+    //             FROM Suministros s
+    //             INNER JOIN Productos p ON s.ProductoID = p.ProductoID
+    //             INNER JOIN ProductosEnSucursal ps ON s.ProductoID = ps.ProductoID AND s.SucursalID = ps.SucursalID
+    //             INNER JOIN Sucursal su ON s.SucursalID = su.SucursalID
+    //             WHERE su.Nombre = '{$_SESSION['user']}' AND p.FamiliaProducto = 'SECOS';
+    //             ";
+    //     break;
+    // case "EPP":
+    //     $sql = "SELECT s.ProductoID AS ID, p.NombreProducto AS Nombre, s.CantidadPasada AS UltimoPedido, s.FechaSuministro AS Fecha, s.Stock AS Stock, s.UnidadMedida AS UnidadMedida
+    //             FROM Suministros s
+    //             INNER JOIN Productos p ON s.ProductoID = p.ProductoID
+    //             INNER JOIN ProductosEnSucursal ps ON s.ProductoID = ps.ProductoID AND s.SucursalID = ps.SucursalID
+    //             INNER JOIN Sucursal su ON s.SucursalID = su.SucursalID
+    //             WHERE su.Nombre = '{$_SESSION['user']}' AND p.FamiliaProducto = 'EPP';
+    //             ";
+    //     break;
+    // case "LIMPIEZA":
+    //     $sql = "SELECT s.ProductoID AS ID, p.NombreProducto AS Nombre, s.CantidadPasada AS UltimoPedido, s.FechaSuministro AS Fecha, s.Stock AS Stock, s.UnidadMedida AS UnidadMedida
+    //             FROM Suministros s
+    //             INNER JOIN Productos p ON s.ProductoID = p.ProductoID
+    //             INNER JOIN ProductosEnSucursal ps ON s.ProductoID = ps.ProductoID AND s.SucursalID = ps.SucursalID
+    //             INNER JOIN Sucursal su ON s.SucursalID = su.SucursalID
+    //             WHERE su.Nombre = '{$_SESSION['user']}' AND p.FamiliaProducto = 'LIMPIEZA';
+    //             ";
+    //     break;
+    // case "SMALLWARE":
+    //     $sql = "SELECT s.ProductoID AS ID, p.NombreProducto AS Nombre, s.CantidadPasada AS UltimoPedido, s.FechaSuministro AS Fecha, s.Stock AS Stock, s.UnidadMedida AS UnidadMedida
+    //             FROM Suministros s
+    //             INNER JOIN Productos p ON s.ProductoID = p.ProductoID
+    //             INNER JOIN ProductosEnSucursal ps ON s.ProductoID = ps.ProductoID AND s.SucursalID = ps.SucursalID
+    //             INNER JOIN Sucursal su ON s.SucursalID = su.SucursalID
+    //             WHERE su.Nombre = '{$_SESSION['user']}' AND p.FamiliaProducto = 'SMALLWARE';
+    //             ";
+    //     break;
+    // case "PRENDAS":
+    //     $sql = "SELECT s.ProductoID AS ID, p.NombreProducto AS Nombre, s.CantidadPasada AS UltimoPedido, s.FechaSuministro AS Fecha, s.Stock AS Stock, s.UnidadMedida AS UnidadMedida
+    //             FROM Suministros s
+    //             INNER JOIN Productos p ON s.ProductoID = p.ProductoID
+    //             INNER JOIN ProductosEnSucursal ps ON s.ProductoID = ps.ProductoID AND s.SucursalID = ps.SucursalID
+    //             INNER JOIN Sucursal su ON s.SucursalID = su.SucursalID
+    //             WHERE su.Nombre = '{$_SESSION['user']}' AND p.FamiliaProducto = 'PRENDAS DE VESTIR';
+    //             ";
+    //     break;
+    // }
 
+    $sql = "SELECT s.ProductoID AS ID, p.NombreProducto AS Nombre, s.CantidadPasada AS UltimoPedido, s.FechaSuministro AS Fecha, s.Stock AS Stock, s.UnidadMedida AS UnidadMedida
+            FROM Suministros s
+            INNER JOIN Productos p ON s.ProductoID = p.ProductoID
+            INNER JOIN ProductosEnSucursal ps ON s.ProductoID = ps.ProductoID AND s.SucursalID = ps.SucursalID
+            INNER JOIN Sucursal su ON s.SucursalID = su.SucursalID
+            WHERE su.Nombre = '{$_SESSION['user']}' AND p.FamiliaProducto = 'SECOS';
+    ";
+     
+     
     $result = sqlsrv_query($conn, $sql);
     if ($result === false) {
     die(print_r(sqlsrv_errors(), true));
